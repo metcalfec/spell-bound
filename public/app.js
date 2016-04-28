12cf11fd7917fd0be131f561b759143f9913e54a
Chris Metcalfe <metcalfe.c@gmail.com>
2016-04-28 15:48:58 -0700

    Clicking letter tiles to form answer is now functional. Letter tiles are removed on click and populate in the answer area. #1

--- public/app.js
+++ public/app.js
@@ -7,12 +7,30 @@ app.$inject = ['$http'];
 function game($http) {
   var vm = this;
   vm.message = "New Game";
+  vm.answerText = "Answer";
+  vm.inputAnswer = [];
+  activate();
 
-  var newWord = $http.get('http://localhost:1337/game');
-  newWord.then(function(info) {
-    vm.word = info.data;
-    vm.letters = info.data.newWordArray;
-    console.log(info.data);
-    console.log(vm.letters);
-  })
+  function activate() {
+    getNewGame();
+  }
+
+  function getNewGame() {
+    var newWord = $http.get('http://localhost:1337/game');
+    newWord.then(function(info) {
+      vm.word = info.data;
+      vm.letters = info.data.wordArray;
+      console.log(info.data);
+      console.log(vm.letters);
+    })
+  }
+
+  vm.answer = function(letter) {
+    var currentIndex = vm.letters.indexOf(letter);
+    // var clickedLetters =
+    vm.inputAnswer.push((vm.letters.splice(currentIndex, 1)).toString());
+    console.log(vm.inputAnswer);
+
+
+  }
 }
