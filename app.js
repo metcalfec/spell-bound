9b10b954bc11d674404c8f7af4918885b2e32297
Chris Metcalfe <metcalfe.c@gmail.com>
2016-04-28 13:30:45 -0700

    The letters of the requested word are randomized on server side. #1

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 var express = require('express');
 var app = express();
 
-var newWordArray = [];
+var array = [];
 
 app.use(express.static('./public'));
 
 app.get('/game', function(req, res) {
   var game = {
     word: 'APPLE',
-    difficulty: 'Medium',
-    newWordArray: newWordArray
+    wordArray: array,
+    image: "http://images.clipartpanda.com/teacher-apple-border-clipart-KTjgkqLTq.jpeg",
+    difficulty: 'Easy'
   }
   letterArray(game.word);
   res.json(game);
 })
 
 function letterArray(word) {
-  newWordArray = [];
+  array = [];
   for (var i = 0; i < word.length; i++) {
-    newWordArray.push(word[i]);
+    array.push(word[i]);
   }
+  randomArray(array);
 }
+
+function randomArray(array) {
+  var x = array.length;
+  var tempVal;
+  var randIndex;
+  while ( 0 !== x) {
+    randIndex = Math.floor(Math.random() * x);
+    x -= 1;
+    tempVal = array[x];
+    array[x] = array[randIndex];
+    array[randIndex] = tempVal;
+  }
+  return array;
+}
+
 
 app.listen(1337, function() {
   console.log("Listening on port 1337");
