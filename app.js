ddee73f6eaf64a0e5edd4a483a761cd944b91054
Chris Metcalfe <metcalfe.c@gmail.com>
2016-05-03 10:00:19 -0700

    Troubleshooting heroku launch error.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var url = 'mongodb://localhost/spell-bound'
 
 var array = [];
 
-app.use(express.static('./public'));
+app.use(express.static('./public/'));
 app.use(cookieParser());
 
 app.get('/login', function(req, res) {
